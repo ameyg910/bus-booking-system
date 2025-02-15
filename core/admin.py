1f58ebce194c02de79f308ae91cf614252f1692c
ameyg910 <80216899+ameyg910@users.noreply.github.com>
2025-02-15 22:43:59 +0530

    Delete core directory

--- core/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import User, Bus, Booking
-
-admin.site.register(User)
-admin.site.register(Bus)
-admin.site.register(Booking)
--- core/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import home, passenger_register, passenger_login, passenger_logout
-
-urlpatterns = [
-    path("", home, name="home"),  # ✅ Fix: Add home page route
-    path("register/", passenger_register, name="passenger_register"),
-    path("login/", passenger_login, name="passenger_login"),
-    path("logout/", passenger_logout, name="passenger_logout"),
-]
